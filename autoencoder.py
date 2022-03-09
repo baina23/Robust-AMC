5910e23230a472459b68c4aef611bf7de747b592
baina23 <yujie.zhang396@duke.edu>
2022-03-09 21:30:30 +0000

    trainable autoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -1,12 +1,12 @@
 import numpy as np
-import seaborn as sns
+#import seaborn as sns
 import _pickle as cPickle
-import matplotlib.pyplot as plt
-%matplotlib inline
+#import matplotlib.pyplot as plt
+#%matplotlib inline
 
 import datetime
 import warnings
-import argparse
+import argparse 
 import os, random
 warnings.filterwarnings("ignore")
 
@@ -21,10 +21,10 @@ from utilities import *
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--gpu", help="index of gpu", type=int, default=-1)
+    parser.add_argument("--gpu", help="index of gpu", type=str, default=-1)
     return parser.parse_args()
-
-os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+args = parse_args()
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Current Device: ", device)
@@ -37,7 +37,7 @@ model = AUTOENCODER()
 model.to(device)
 model_checkpoint = "AUTOENCODER.pt"
 
-x_train, x_test, y_train, y_test, test_labels, test_idx = train_test_split(X, labels, mods, NN = curr_model)
+x_train, x_test, y_train, y_test, test_labels, test_idx = train_test_split(X, labels, mods)
 
 train_dataset = TensorDataset(x_train, x_train)
 test_dataset  = TensorDataset(x_test,  x_test)
@@ -50,30 +50,30 @@ num_epochs = 100
 criterion  = nn.MSELoss()
 optimizer  = optim.SGD(model.parameters(), lr = 0.001)
 
-best_val_acc = 0.
+best_val_loss = 1.
 
 for epoch in range(num_epochs) :
     model.train()
     train_epoch_loss = 0.
     train_epoch_acc  = 0.
-    for batch_idx, (data, labels) in enumerate(TrainLoader) :
+    for batch_idx, (data, lbs) in enumerate(TrainLoader) :
         data   = data.to(device)
-        labels = labels.to(device)
+        lbs = lbs.to(device)
         
         optimizer.zero_grad()
         outputs = model(data)
-        loss    = criterion(outputs, labels)
-        acc     = evaluate_accuracy(outputs, labels)
+        loss    = criterion(outputs, lbs)
+        #acc     = evaluate_accuracy(outputs, lbs)
         loss.backward()
         optimizer.step()
         
         train_epoch_loss += loss.item()
-        train_epoch_acc  += acc.item()
+        #train_epoch_acc  += acc.item()
     
     with torch.no_grad() :
         model.eval()
         val_epoch_loss = 0.
-        val_epoch_acc  = 0.
+        #val_epoch_acc  = 0.
         
         for X_val_batch, y_val_batch in TestLoader :
             X_val_batch = X_val_batch.to(device)
@@ -82,21 +82,21 @@ for epoch in range(num_epochs) :
             y_val_pred = model(X_val_batch)
             
             val_loss = criterion(y_val_pred, y_val_batch)
-            val_acc  = evaluate_accuracy(y_val_pred, y_val_batch)
+            #val_acc  = evaluate_accuracy(y_val_pred, y_val_batch)
             
             val_epoch_loss += val_loss.item()
-            val_epoch_acc  += val_acc.item()
+            #val_epoch_acc  += val_acc.item()
     
     avg_train_loss = float(train_epoch_loss) / len(TrainLoader)
-    avg_train_acc  = float(train_epoch_acc) / len(TrainLoader)
+    #avg_train_acc  = float(train_epoch_acc) / len(TrainLoader)
     avg_val_loss   = float(val_epoch_loss) / len(TestLoader)
-    avg_val_acc    = float(val_epoch_acc) / len(TestLoader)
+    #avg_val_acc    = float(val_epoch_acc) / len(TestLoader)
     
-    print(f'Epoch {epoch+1}: | Train Acc: {avg_train_acc:.3f} | Test Acc: {avg_val_acc:.3f}')
+    print(f'Epoch {epoch+1}: | Train Loss: {avg_train_loss:.4e} | Test Loss: {avg_val_loss:.4e}')
     
-    if avg_val_acc > best_val_acc :
+    if avg_val_loss < best_val_loss :
         print("Saving Model Checkpoint......")
-        best_val_acc = avg_val_acc
+        best_val_loss = avg_val_loss
         torch.save(model.state_dict(), model_checkpoint)
 
 print("Training Complete!")
